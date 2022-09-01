a6a158a2e7aec9c28962099344e858cc71290203
Nick Ghezzi <toastyplaster@gmail.com>
2022-09-01 08:27:31 -0400

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(phrase, num) {
+    //doing it with just arrays
+    // const words = new Array(num);
+    // words.fill(phrase);
+    // return words.join("");
 
+    //doing it with just string
+    // return phrase.repeat(num);
+
+    //doing it with loops
+    let words = phrase;
+    for(let i = 0; i < num - 1; i++){
+        words += phrase;
+    }
+    return words;
 };
 
 // Do not edit below this line
