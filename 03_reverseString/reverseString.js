53a4735a228e4bbd7052d00bee14b0907a5ccda9
Nick Ghezzi <toastyplaster@gmail.com>
2022-09-02 11:31:01 -0400

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (phrase) {
+    let reversePhrase = "";
+    for (let i = 0; i < phrase.length; i++) {
+        reversePhrase = reversePhrase.concat(phrase.charAt((phrase.length - 1) - i));
+    }
 
+    return reversePhrase;
 };
 
 // Do not edit below this line
