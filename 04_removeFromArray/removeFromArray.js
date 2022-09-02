d045b629cd4ffc874047d7e481a84ebc65ddb6eb
Nick Ghezzi <toastyplaster@gmail.com>
2022-09-02 12:21:36 -0400

    Completed exersice 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...badArgs) {
+    
+    for(let i = 0; i < badArgs.length; i++){
+        for(let j = 0; j < array.length; j++){
+            if(array[j] === badArgs[i]){
+                array.splice(j, 1);
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
