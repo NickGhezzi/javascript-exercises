9dca698a38958014136f4c90e26442d4afe83920
Nick Ghezzi <toastyplaster@gmail.com>
2022-09-03 00:23:25 -0400

    Almost done exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    //const nums = [];
+    let sum = 0;
+    const range = Math.abs(end - start);
+    for(let i = 0; i <= range; i++){
+        sum += Math.abs(end - (range - i));
+        //console.log(sum);
+    }
 
+    // for(let num of nums){
+    //     sum += num;
+    // }
+    return sum;
+
+    //nums[i] = Math.abs(end - (range - i));
 };
 
 // Do not edit below this line
